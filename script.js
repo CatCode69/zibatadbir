e28deb33efb303078dabfce3034bdc5209fefeac
CatCode69 <86779030+CatCode69@users.noreply.github.com>
2022-06-06 15:21:44 +0430

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-const gague = document.querySelector(".gague");
-console.log(gague);
-gague.addEventListener("click", function (e) {
-  e.preventDefault();
-});
+// const gague = document.querySelector(".gague");
+// // console.log(gague);
+// gague.addEventListener("keydown", function (e) {
+//   if (e.key === "Enter") {
+//     e.preventDefault();
+//   }
+// });
+
+// const btnSubmit = document.querySelector(".btn-submit");
+
+// // btnSubmit.addEventListener("click", function (e) {
+// //   e.preventDefault();
+// // });
