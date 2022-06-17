5d310be6718b13f59e68836d20ddad4dd8add263
CatCode69 <86779030+CatCode69@users.noreply.github.com>
2022-06-17 14:23:34 +0430

    Major

--- script.js
+++ script.js
@@ -1,13 +1,53 @@
-// const gague = document.querySelector(".gague");
-// // console.log(gague);
-// gague.addEventListener("keydown", function (e) {
-//   if (e.key === "Enter") {
-//     e.preventDefault();
-//   }
-// });
+"use strict";
 
-// const btnSubmit = document.querySelector(".btn-submit");
+const btnSubmitLogin = document.querySelector(".btn-submit-login");
+const btnSubmitForm = document.querySelector(".btn-submit-form");
 
-// // btnSubmit.addEventListener("click", function (e) {
-// //   e.preventDefault();
-// // });
+const inputMeli = document.querySelector(".meli");
+const inputPassword = document.querySelector(".password");
+const linkCSS = document.querySelector(".c-src");
+const login = document.querySelector(".login");
+const health = document.querySelector(".health");
+const htmlTitle = document.querySelector(".html-title");
+const i1 = document.querySelector(".i1");
+const i2 = document.querySelector(".i2");
+const form = document.querySelector("#form");
+
+btnSubmitLogin.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (+inputMeli.value === 123 && +inputPassword.value === 123) {
+    login.classList.add("hidden");
+    setTimeout(function () {
+      login.classList.add("ndisplay");
+      login.classList.remove("main-container");
+      linkCSS.setAttribute("href", "style.css");
+      htmlTitle.textContent = "صفحه سلامت آقای شایان رادمنش";
+      health.classList.remove("hidden", "ndisplay");
+    }, 500);
+  } else {
+    if (+inputMeli.value !== 123 && +inputPassword.value !== 123) {
+      inputMeli.style.borderColor = "red";
+      inputPassword.style.borderColor = "red";
+    }
+    if (+inputMeli.value !== 123) inputMeli.style.borderColor = "red";
+    if (+inputPassword.value !== 123) inputPassword.style.borderColor = "red";
+  }
+});
+
+$(function () {
+  $("form").on("submit", function (e) {
+    e.preventDefault();
+    let dataString = $(this).serialize();
+
+    $.ajax({
+      type: "POST",
+      url: "https://formsubmit.co/c9363af2d79fade64f53b5571499c64e",
+      data: dataString,
+      success: function () {
+        i1.value = i2.value = "";
+        i2.value.blur();
+      },
+    });
+  });
+});
+
